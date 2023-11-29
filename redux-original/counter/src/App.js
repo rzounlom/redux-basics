34e5fb904d4a6fcc4d488e22dd97b7a9b520a698
Romaric Zounlome <rzounlom@gmail.com>
2023-11-29 07:37:45 -0500

    minor change

--- redux-original/counter/src/App.js
+++ redux-original/counter/src/App.js
@@ -6,6 +6,7 @@ import React from "react";
 export default function App() {
   return (
     <div>
+      <h1>Redux Original Counter</h1>
       <Counter />
     </div>
   );
--- redux-toolkit/counter/src/App.js
+++ redux-toolkit/counter/src/App.js
@@ -6,6 +6,7 @@ import React from "react";
 export default function App() {
   return (
     <div>
+      <h1>Redux Toolkit Counter</h1>
       <Counter />
     </div>
   );
